1c4a5ff0ce74388194ebbf36764544096c0d6218
novita <ekasarinovita4@gmail.com>
2023-08-19 20:24:01 +0700

    update assigment

--- biodata.go
+++ biodata.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Person struct {
 	name string
@@ -12,20 +15,25 @@ type Person struct {
 func main() {
 
 	 biodata := []Person{
-		{name : "Boy Valentyas", addres: "Jakarta Timur", job: "Programmer", reason: "Karena tertarik dengan Golang"},
-		{name : "Sebastian Agustus", addres: "Kelapa Gading", job: "Quality Tester", reason: "Karena tertarik dengan Golang"},
-		{name : "Noventi Agt", addres: "Papua", job: "Front End Programmer", reason: "Karena tertarik dengan Golang"},
-		{name : "Novita Ekasari", addres: "Jakarta Timur", job: "Programmer", reason: "Karena tertarik dengan Golang"},
+		{name : "Boy", addres: "Jakarta Timur", job: "Programmer", reason: "Karena tertarik dengan Golang"},
+		{name : "Sebastian", addres: "Kelapa Gading", job: "Quality Tester", reason: "Karena tertarik dengan Golang"},
+		{name : "Noventi", addres: "Papua", job: "Front End Programmer", reason: "Karena tertarik dengan Golang"},
+		{name : "Novita", addres: "Jakarta Timur", job: "Programmer", reason: "Karena tertarik dengan Golang"},
 		
 	}
 
+	var biodata1 = os.Args[1]
+
 	for i, v := range biodata {
+		if biodata1 == v.name {
 		fmt.Println("id:", i)
 			fmt.Println("name :", v.name)
 			fmt.Println("addres :", v.addres)
 			fmt.Println("job :", v.job)
 			fmt.Println("reason :", v.reason)
-	}
-
+		}else{
+			fmt.Println("data tidak ada")
+		}
+	} 
 }	
 
